8c5936bab7150974804585935fb7c0c88bff2bed
Adam Beer <awebeer256@users.noreply.github.com>
2017-03-13 16:51:57 -0400

    Created skeleton (#3)
    * GLOBAL has GameMgr component
    * GameMgr has game state enum definition, property, and skeleton of advancing method

--- Assets/GameMgr.cs
+++ Assets/GameMgr.cs
@@ -4,5 +4,43 @@ using UnityEngine;
 
 public class GameMgr : MonoBehaviour {
 
+	public enum GameState {
+		INVALID = -1,
+		NONE = 0,
 
+		TITLE_MENUS,
+		IN_GAME,
+		GAME_OVER
+	}
+
+	private GameState state;
+	public GameState State {
+		private set { state = value; }
+		get { return state; }
+	}
+
+	void Start() {
+		State = GameState.TITLE_MENUS;
+	}
+
+	/// <summary>
+	/// Advances game to the next state, handling the associated transitions.
+	/// </summary>
+	/// <returns><c>true</c>, if state was advanced, <c>false</c> otherwise.</returns>
+	public bool AdvanceState() {
+		if (State == GameState.INVALID || State == GameState.NONE) {
+			Debug.LogError ("Attempted to advance game state while current state was " + State);
+			return false;
+		} else if (State == GameState.GAME_OVER) {
+			return false;
+		}
+
+
+		//state transition logic goes here
+
+
+		//temporary
+		Debug.LogError("Reached incomplete section of GameMgr.AdvanceState()");
+		return false;
+	}
 }
