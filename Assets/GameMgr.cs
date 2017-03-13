11d2b02274947d98e73014aaacf592afd911ac1c
Adam Beer <a.beer_256@live.ca>
2017-03-13 15:15:07 -0400

    Revert "cleared out defaut start and update methods"
    This reverts commit cc796953d4a473f1dcd21df19b52e81f8a8cb89b.

--- Assets/GameMgr.cs
+++ Assets/GameMgr.cs
@@ -4,5 +4,13 @@ using UnityEngine;
 
 public class GameMgr : MonoBehaviour {
 
-
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
 }
