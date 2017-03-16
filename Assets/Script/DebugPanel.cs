1432618603123dbb83a60176e4f827bc9335dc93
Adam Beer <awebeer256@users.noreply.github.com>
2017-03-16 12:23:10 -0400

    Created script

---
+++ Assets/Script/DebugPanel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Attached to the debug panel; drives its behaviour.
+/// </summary>
+public class DebugPanel : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
