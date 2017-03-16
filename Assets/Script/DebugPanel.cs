dc64553dfbdb2fdede26085782b6b5cd4188c66f
Adam Beer <awebeer256@users.noreply.github.com>
2017-03-16 12:32:05 -0400

    attached ref to gametime to script

--- Assets/Script/DebugPanel.cs
+++ Assets/Script/DebugPanel.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 /// <summary>
 /// Attached to the debug panel; drives its behaviour.
 /// </summary>
 public class DebugPanel : MonoBehaviour {
+
+	public Text timedisp;
 
 	// Use this for initialization
 	void Start () {
