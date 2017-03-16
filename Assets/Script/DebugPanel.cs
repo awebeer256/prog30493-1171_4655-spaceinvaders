1de01f24595ebee8faf0e7a27f7c455023eb549d
Adam Beer <awebeer256@users.noreply.github.com>
2017-03-16 12:53:33 -0400

    populated panel and script (#4)
    * Added static text
    * Added game-time text
    * attached ref to gametime to script
    * made update method signature

--- Assets/Script/DebugPanel.cs
+++ Assets/Script/DebugPanel.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 /// <summary>
 /// Attached to the debug panel; drives its behaviour.
 /// </summary>
 public class DebugPanel : MonoBehaviour {
+
+	public Text timeDisp;
 
 	// Use this for initialization
 	void Start () {
@@ -14,6 +17,10 @@ public class DebugPanel : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		PanelUpdate ();
+	}
+
+	private void PanelUpdate() {
+
 	}
 }
