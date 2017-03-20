506dc1ca11e427dfc92761501f84ab090110576a
Adam Beer <awebeer256@users.noreply.github.com>
2017-03-20 10:52:43 -0400

    Created debug panel (#6)
    It shows the amount of time elapsed in-game. More data to come later

---
+++ Assets/Script/DebugPanel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Attached to the debug panel; drives its behaviour.
+/// </summary>
+public class DebugPanel : MonoBehaviour {
+
+	public Text timeDisp;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		PanelUpdate ();
+	}
+
+	private void PanelUpdate() {
+		timeDisp.text = string.Format ("{0:N2}", Time.time);
+	}
+}
