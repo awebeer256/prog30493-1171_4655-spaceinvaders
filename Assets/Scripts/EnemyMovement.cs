a8a6a8da51d90369a5a5f8e27a6c8f1b6cedefe3
EhJackson88 <andrew.donald.jackson@gmail.com>
2017-03-27 09:08:15 -0400

    Started Script for enemy movement
    - Added 2 blocks to start and stop enemy movement
    - Added script to move enemy between blocks

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour {
+
+	public GameObject Enemy; //enemy to hold the script
+	public float movementSpeed; //speed at which the enemies move
+	private  Transform currentPoint; //current point enemy is at
+	public Transform[] movementPoints; //array to hold two points that enemies will move between
+	public int pointSelection; //used to change the points enemies will move too
+
+	void EnemyMovementUpdate(){
+
+		Enemy.transform.position = Vector3.MoveTowards (Enemy.transform.position, currentPoint.transform.position, Time.deltaTime * movementSpeed);
+
+		if (Enemy.transform.position == currentPoint.position) {
+			pointSelection++;
+			if (pointSelection == movementPoints.Length) {
+				pointSelection = 0;
+			}
+
+			currentPoint = movementPoints [pointSelection];
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		currentPoint = movementPoints[pointSelection];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		EnemyMovementUpdate ();
+	}
+}
