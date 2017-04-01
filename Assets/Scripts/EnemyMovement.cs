7fa4ca9d2aac71f555bef5907fdae7b042894b4f
EhJackson88 <andrew.donald.jackson@gmail.com>
2017-04-01 14:56:22 -0400

    Finalized simple enemy movement
    - enemies move left and right at speed of 0.65f
    - movement is controlled by dir which changes based on position of x for
    enemy wave
    - enemies move down 0.5f when reaching left or right edge of screen

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -4,26 +4,33 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour {
 
-	public int dir = 1; //changes direction of enemies
-	private int movementSpeed = 2; //controls the speed of the enemies 
+	/*
+	This class is designed to move the enemies left and right as well as closer to the player
+	direction is based on x position of enemy wave, enemies continue to move down regardless of where they are
+	going to set onCollisionEnter script on sheilds to check for collisions w/ enemies to end game
+	speed is set atm but should be changed based on number of enemies
+	possibly every 10 enemies destoryed movementspeed += 0.05f
+	*/
 
+	public int dir = 1; //changes direction of enemies
+	private float movementSpeed = 0.65f; //controls the speed of the enemies
 
 	void EnemyMovementUpdate(){
 
-		transform.Translate (Vector3.right * dir * movementSpeed * Time.deltaTime);
+		transform.Translate (Vector3.right * dir * movementSpeed * Time.deltaTime); //moves enemies based on direction and speed of enemies
 
-		if (transform.position.x >= 3.5) {
-			dir = -1;
-			//transform.position.y = transform.position.y - 1;
-		} else if (transform.position.x <= -3.5) {
-			dir = 1;
-			//transform.position.y = transform.position.y - 1;
+		if (transform.position.x >= 3.25) {
+			dir = -1; //if the enemy wave changes direction based on x position and moves down 0.5f towards player
+			transform.position = new Vector3 (transform.position.x, transform.position.y - 0.5f, transform.position.z);
+		} else if (transform.position.x <= -3.25) {
+			dir = 1; //changes direction of enemies again based on pos of x, also moves enemies down towards player
+			transform.position = new Vector3 (transform.position.x, transform.position.y - 0.5f, transform.position.z);
 		}
 	}
 
 	// Use this for initialization
 	void Start () {
-		//currentPoint = movementPoints[pointSelection];
+		
 	}
 	
 	// Update is called once per frame
