b2307fb70bd44563be75321e5e7f4118a9597ef2
Maitri Patel <maitri_1997@yahoo.com>
2017-04-02 14:19:28 -0400

    created panel for titlescreen

--- Assets/Script/UIMgr.cs
+++ Assets/Script/UIMgr.cs
@@ -1,16 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIMgr : MonoBehaviour {
+
+	public enum eUIMgrState
+	{
+		Invalid = -1,
+		None = 0,
+		TitleScreen,
+		LevelSelect,
+		InGame
+	};
+
+	public List<isaUIPanel> panels;
+	public eUIMgrState state;
+	private eUIMgrState prevState;
+
+
+
 
 	// Use this for initialization
 	void Start () {
 		
 	}
+
+	public void UIMgrUpdate()
+	{
+		prevState = state;
+	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+		UIMgrUpdate ();
 	}
 }
