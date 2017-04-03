097019ad8498335201d8328a2252b3442550bf37
Adam Beer <awebeer256@users.noreply.github.com>
2017-04-03 10:26:09 -0400

    manually dragged stuff from new-enemymovement
    that's commit was 7fa4ca9d2aac71f555bef5907fdae7b042894b4f

---
+++ Assets/Script/EnemyMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour {
+
+	/*
+	This class is designed to move the enemies left and right as well as closer to the player
+	direction is based on x position of enemy wave, enemies continue to move down regardless of where they are
+	going to set onCollisionEnter script on sheilds to check for collisions w/ enemies to end game
+	speed is set atm but should be changed based on number of enemies
+	possibly every 10 enemies destoryed movementspeed += 0.05f
+	*/
+
+	public int dir = 1; //changes direction of enemies
+	private float movementSpeed = 0.65f; //controls the speed of the enemies
+
+	void EnemyMovementUpdate(){
+
+		transform.Translate (Vector3.right * dir * movementSpeed * Time.deltaTime); //moves enemies based on direction and speed of enemies
+
+		if (transform.position.x >= 3.25) {
+			dir = -1; //if the enemy wave changes direction based on x position and moves down 0.5f towards player
+			transform.position = new Vector3 (transform.position.x, transform.position.y - 0.5f, transform.position.z);
+		} else if (transform.position.x <= -3.25) {
+			dir = 1; //changes direction of enemies again based on pos of x, also moves enemies down towards player
+			transform.position = new Vector3 (transform.position.x, transform.position.y - 0.5f, transform.position.z);
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		EnemyMovementUpdate ();
+	}
+}
