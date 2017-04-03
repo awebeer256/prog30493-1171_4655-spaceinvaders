840f2ccf9300bf7c91feeaebabd45247b9588f22
Adam Beer <awebeer256@users.noreply.github.com>
2017-04-03 09:42:30 -0400

    Added PC idle & movement (#17)
    * Imported walking animation
    * Added idle animation
    * applied idle anim (#14)
    * Fixed pc physics
    * Added walking animations
    * Replaced idle and walking animations
    * Added left/right controls
    * made deletion of unused assets stick
    * Made animations more responsive

---
+++ Assets/Script/PCControl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PCControl : MonoBehaviour {
+
+	private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+		if (anim == null)
+			Debug.LogError (string.Format("{0}: couldn't find an Animator", this.GetType ().ToString ()));
+	}
+	
+	void FixedUpdate() {
+		HandleInputs ();
+	}
+
+	/// <summary>
+	/// Updates PC behaviour based on player input.
+	/// </summary>
+	private void HandleInputs() {
+		anim.SetBool ("walkLeft", Input.GetAxisRaw ("Horizontal") < -0.5);
+		anim.SetBool ("walkRight", Input.GetAxisRaw ("Horizontal") > 0.5);
+	}
+}
