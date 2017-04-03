0e0c3e7bdf01ee68204f602c7431b5d8aa657723
Maitri Patel <map97@users.noreply.github.com>
2017-04-03 09:32:49 -0400

    Scratch start gui (#16)
    * created gui script
    
    * added UIMgr script to GLOBAL
    
    * added isaUIPanel script
    
    * created panel for titlescreen
    
    * added level-select panel
    
    * added quit-panel
    
    * created sound effects folder and canvas prefab

---
+++ Assets/Script/UIMgr.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIMgr : MonoBehaviour {
+
+	public enum eUIMgrState
+	{
+		Invalid = -1,
+		None = 0,
+		TitleScreen,
+		LevelSelect,
+		InGame,
+
+		FirstState = TitleScreen,
+		LastState = InGame
+		//NumStates = ((LastState - FirstState) + 1)
+	};
+
+	public List<isaUIPanel> panels;
+	public eUIMgrState state;
+	private eUIMgrState prevState;
+
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	public void IncState()
+	{
+		state++;
+		if (state > eUIMgrState.LastState) {
+			state = eUIMgrState.LastState;
+		}
+		
+	}
+
+	public void DecState()
+	{
+		state--;
+		if (state < eUIMgrState.FirstState) {
+			state = eUIMgrState.FirstState;
+		}
+
+	}
+	public void ShowCurrentPanel() 
+	{
+		foreach (isaUIPanel iap in panels) 
+		{
+			if (iap.state == state) {
+				iap.gameObject.SetActive (true);
+			} else {
+				iap.gameObject.SetActive (false);
+			}
+				
+		}
+	}
+
+	public void UIMgrUpdate()
+	{
+		if (state != prevState)
+		{
+			ShowCurrentPanel ();
+		}
+
+		prevState = state;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		UIMgrUpdate ();
+	}
+}
