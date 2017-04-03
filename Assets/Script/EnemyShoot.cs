ec4d4e67ee886d40dfb684ec4429a9ee1a859467
EhJackson88 <andrew.donald.jackson@gmail.com>
2017-04-03 11:58:16 -0400

    Playing with enemy shooting
    - added basic enemy shooting script
    - added bullet for enemies
    - chnaged color of bullets
    - playing around w/ timer for shooting

---
+++ Assets/Script/EnemyShoot.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShoot : MonoBehaviour {
+
+	public GameObject enemyBullet;
+	public float shootTime;
+	public float timeCounter;
+
+	void TimeCheck(){
+		timeCounter = 0.0f;
+		shootTime = Random.Range (3, 20);
+	}
+
+	void EnemyShootingUpdate(){
+		timeCounter += Time.deltaTime;
+		if (timeCounter >= shootTime) {
+			Instantiate (enemyBullet, transform.position, Quaternion.identity);
+			TimeCheck ();
+		}
+
+	}
+
+	// Use this for initialization
+	void Start () {
+		TimeCheck ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		EnemyShootingUpdate ();
+	}
+}
