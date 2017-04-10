0f52168158fc19e6466bd4d658412deafe0e0c73
EhJackson88 <andrew.donald.jackson@gmail.com>
2017-04-10 11:46:12 -0400

    Basic Enemy shooting logic
    - Bullet prefab given rigidbody / EnemyBulletLogic
    - Created script for enemy bullet logic
    - check to see if bullet collides w/ sheild or player
    - destory bullet whenever it collides with anything
    - Added rigid body to bottom boundary to check for collision
    - Created script - isAShield and placed on sheild for collision
    detection
    - Created script- isACharacter / isPlayerOwned and added to player to
    detect collision

---
+++ Assets/Script/EnemyBulletLogic.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBulletLogic : MonoBehaviour {
+
+	/*
+	 * This class is designed to destory the enemybullet when it collides with anything
+	 * will check to see if bullet hits: shields or Player
+	 * if shield - destory whichever cube was hit in the shield
+	 * if player - reduce live, respawn player 
+	 */
+
+	public GameObject enemyBullet;
+
+	void OnCollisionEnter(Collision collision){
+		if (collision.gameObject.GetComponent<isAShield> () != null) {
+			//destory one block per sheild
+		} else if (collision.gameObject.GetComponent<isCharacter> () != null 
+			&& collision.gameObject.GetComponent<isPlayerOwned>() != null) {
+			//check game manager for player lives,
+			//reduce number of lives for player,
+			//respawn player in middle of screen 
+		}
+		Destroy (gameObject); //gameobject is destroyed when it collides with anything 
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
