dc7f41480aa43ae0e0b8dfd93090ac0e200b21a6
Adam Beer <awebeer256@users.noreply.github.com>
2017-04-14 17:07:56 -0400

    Squash merge from new-enemyShoot
    Enemies now shoot. Bullets destroy themselves if they hit anything.

---
+++ Assets/Script/EnemyShoot.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShoot : MonoBehaviour {
+
+	public GameObject enemyBullet;
+	public float shootTime;
+	public float timeCounter;
+	public GameObject enemyBlock;
+
+	void TimeCheck(){
+		timeCounter = 0.0f;
+		shootTime = Random.Range (3, 15);
+	}
+
+	void EnemyShootingUpdate(){
+		timeCounter += Time.deltaTime;
+		if (timeCounter >= shootTime) {
+			if (enemyBlock == null) {
+				Instantiate (enemyBullet, transform.position, Quaternion.identity);
+				TimeCheck ();
+			}
+		}
+
+	}
+
+	// Use this for initialization
+	void Start () {
+		TimeCheck ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		EnemyShootingUpdate ();
+	}
+}
