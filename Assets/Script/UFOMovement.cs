39594bf6658c7c0b64897c996813aa610d86eabb
Maitri Patel <map97@users.noreply.github.com>
2017-04-16 12:30:00 -0400

    ufo enemy moves along the screen (#29)
    * added UFO movement script
    
    
    * ufo moves across the screen, 
    back and forth
    on an average, UFO crosses the screen 4 times until other enemies reach to the bottom

---
+++ Assets/Script/UFOMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UFOMovement : MonoBehaviour {
+
+
+	public int rot = 1; //changes direction of enemies
+	private float movementSpeed = 3.0f; //controls the speed of the enemies
+
+	void UFOMovementUpdate() {
+
+		transform.Translate (Vector3.up * rot * movementSpeed * Time.deltaTime); //moves enemies based on direction and speed of enemies
+
+		if (transform.position.x <= -30) {
+			rot = -1;
+			transform.Rotate (90, 90, 0);
+			transform.position = new Vector3 (transform.position.x, transform.position.y, transform.position.z);
+
+		} else if (transform.position.x >= 30) {
+			rot = 1;
+			transform.Rotate (90, 270, 0);
+			transform.position = new Vector3 (transform.position.x, transform.position.y, transform.position.z);
+
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		UFOMovementUpdate ();
+	}
+}
