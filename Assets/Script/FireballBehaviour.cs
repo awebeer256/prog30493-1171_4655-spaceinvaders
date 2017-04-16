58357c090145381fd1dd72a5d2b686a1891ff561
Adam Beer <awebeer256@users.noreply.github.com>
2017-04-16 19:18:05 -0400

    made fireball call GameMgr method to increase score
    instead of modifying variable directly

--- Assets/Script/FireballBehaviour.cs
+++ Assets/Script/FireballBehaviour.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class FireballBehaviour : MonoBehaviour {
 
 	private int addScore = 10;
+	private GameObject globalgo;
 
 	// Use this for initialization
 	void Start () {
-		
+		globalgo = GameObject.Find ("GLOBAL");
+		if (globalgo == null)
+			Debug.LogError ("Fireball couldn't find GLOBAL.");
 	}
 	
 	// Update is called once per frame
@@ -18,7 +21,7 @@ public class FireballBehaviour : MonoBehaviour {
 
 	void OnCollisionEnter(Collision collision) {
 		if (collision.gameObject.GetComponent<IsACharacter> () != null && collision.gameObject.GetComponent<IsAEnemyOwned> () != null) {
-			GameMgr.playerScore += addScore;
+			globalgo.GetComponent<GameMgr> ().AddToScore (addScore);
 		}
 		Destroy (gameObject);
 	}
