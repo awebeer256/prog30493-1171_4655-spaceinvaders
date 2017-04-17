ccd0799bd5685141ea5fd212049857712568e8bd
Adam Beer <awebeer256@users.noreply.github.com>
2017-04-17 00:04:18 -0400

    created many aspects of UI
    *Title screen
    *Level select screen
    *Quit button (causes deliberate game over)
    *Nicer score display
    
    Still need game-over and high-score screens, at least.

---
+++ Assets/Script/UIMgr.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+//using System.Web.UI.WebControls;
+
+public class UIMgr : MonoBehaviour {
+
+	public enum eUIMgrState
+	{
+		Invalid = -1,
+		None = 0,
+		TitleScreen,
+		LevelSelect,
+		HighScore,
+		InGame,
+		GameOver
+	};
+
+	public List<IsAUIPanel> panels;
+	public eUIMgrState state;
+
+	private GameMgr gameMgr;
+
+	// Use this for initialization
+	void Start () {
+		gameMgr = GetComponent<GameMgr> ();
+		if (gameMgr == null)
+			Debug.LogError ("UIMgr couldn't find GameMgr.");
+	}
+
+	public void ShowCurrentPanel() 
+	{
+		foreach (IsAUIPanel iap in panels) 
+		{
+			if (iap.state == state) {
+				iap.gameObject.SetActive (true);
+			} else {
+				iap.gameObject.SetActive (false);
+			}
+
+		}
+	}
+
+	private void GoTo(eUIMgrState destState) {
+		state = destState;
+		ShowCurrentPanel ();
+	}
+
+	public void GoToTitle() {
+		GoTo (eUIMgrState.TitleScreen);
+	}
+
+	public void GoToLevelSelect() {
+		GoTo (eUIMgrState.LevelSelect);
+	}
+
+	public void GoToHighScore() {
+		GoTo (eUIMgrState.HighScore);
+	}
+
+	public void GoToInGame() {
+		GoTo (eUIMgrState.InGame);
+	}
+
+	public void GoToGameOver() {
+		GoTo (eUIMgrState.GameOver);
+	}
+}
