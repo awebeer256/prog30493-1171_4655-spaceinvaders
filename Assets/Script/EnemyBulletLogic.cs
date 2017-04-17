69d2d05f9bcfc04e9374cd8701b56ea9f361ab60
EhJackson88 <andrew.donald.jackson@gmail.com>
2017-04-16 21:20:10 -0400

    Added line to reduce lives in player hit
    - If collision detected between enemy shot and player, player loses 1
    life

--- Assets/Script/EnemyBulletLogic.cs
+++ Assets/Script/EnemyBulletLogic.cs
@@ -32,6 +32,7 @@ public class EnemyBulletLogic : MonoBehaviour {
 			//destory one block per sheild
 		} else if (collision.gameObject.GetComponent<IsACharacter> () != null 
 			&& collision.gameObject.GetComponent<IsAPlayerOwned>() != null) {
+			globalgo.GetComponent<GameMgr> ().ReduceLives (reduceLife);
 			//reduce number of lives for player
 			//respawn player in middle of screen 
 		}
